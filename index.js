78028d763fcb7bc45db90c3a22b7224487c22e9a
Allan Robert Tan <92196359+eillanrt@users.noreply.github.com>
2022-05-16 23:06:05 +0800

    Update index.js
    Added useEffect and utilized accurateInterval instead of setInterval to prevent interval from drifting away

--- index.js
+++ index.js
@@ -1,86 +1,117 @@
-const { useState: state } = React;
+const { useState, useEffect } = React;
 const beep = document.getElementById("beep");
 
-const Timer = () => {
-  const [time, setTime] = state(1500),
-    [isRunning, setIsRunning] = state(false),
-    [interval, updateInterval] = state(undefined),
-    [sessionLength, setSessionLength] = state(25),
-    [breakLength, setBreakLength] = state(5),
-    [status, setStatus] = state("session");
+const accurateInterval = function (fn, time) {
+  var cancel, nextAt, timeout, wrapper;
+  nextAt = new Date().getTime() + time;
+  timeout = null;
+  wrapper = function () {
+    nextAt += time;
+    timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+    return fn();
+  };
+  cancel = function () {
+    return clearTimeout(timeout);
+  };
+  timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+  return {
+    cancel: cancel
+  };
+};
 
-  function start_pause(event) {
+const Timer = () => {
+  const [time, setTime] = useState(1500),
+    [sessionLength, setSessionLength] = useState(25),
+    [breakLength, setBreakLength] = useState(5),
+    [isRunning, setIsRunning] = useState(false),
+    [interval, updateInterval] = useState(undefined),
+    [status, setStatus] = useState("focus");
+
+  useEffect(() => {
+    const body = document.body;
+    const timerWrapper = document.getElementById("timer-wrapper");
+
+    if (status === "focus") {
+      body.style.backgroundColor = "#cd5c5c";
+      timerWrapper.style.backgroundColor = "#b22222";
+    } else {
+      body.style.backgroundColor = "#4f666a";
+      timerWrapper.style.backgroundColor = "#2f4f4f";
+    }
+  }, [status]);
+
+  const play_pause = () => {
     let sec = time;
     let stats = status;
 
-    function countDown() {
+    const countDown = () => {
       sec--;
       setTime(sec);
       if (sec === -1) {
         beep.play();
-        if (stats === "session") {
+        if (stats === "focus") {
           setStatus("break");
 
           stats = "break";
           sec = breakLength * 60;
-        } else if (stats === "break") {
-          setStatus("session");
+        } else {
+          setStatus("focus");
 
-          stats = "session";
+          stats = "focus";
           sec = sessionLength * 60;
         }
         setTime(sec);
       }
-    }
+    };
 
     if (!isRunning) {
       setIsRunning(true);
-      updateInterval(setInterval(countDown, 1000));
+      updateInterval(accurateInterval(countDown, 1000));
     } else {
       setIsRunning(false);
-      clearInterval(interval);
+      interval.cancel();
     }
-  }
+  };
 
-  function reset(event) {
+  const reset = () => {
     if (!beep.paused) {
       beep.pause();
       beep.currentTime = 0;
     }
 
-    if (interval !== undefined) {
-      clearInterval(interval);
+    if (interval) {
+      interval.cancel();
       updateInterval(undefined);
     }
 
     setSessionLength(25);
     setBreakLength(5);
     setTime(1500);
-    setStatus("session");
+    setStatus("focus");
     setIsRunning(false);
-  }
+  };
 
-  function clockify() {
-    let minutes = Math.floor(time / 60);
-    let seconds = time - minutes * 60;
+  const clockify = (secs) => {
+    let minutes = Math.floor(secs / 60);
+    let seconds = secs - minutes * 60;
 
     seconds = seconds < 10 ? "0" + seconds : seconds;
     minutes = minutes < 10 ? "0" + minutes : minutes;
 
     return minutes + ":" + seconds;
-  }
+  };
+
+  const flex_SpaceAround = {
+    display: "flex",
+    justifyContent: "space-around"
+  };
 
   return (
     <div id="timer">
       <div id="control-timers">
         <div id="session-label">
-          <h1>Session Length</h1>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-around",
-            }}
-          >
+          <h1>Focus Length</h1>
+          <div style={flex_SpaceAround}>
             <button
               id="session-increment"
               onClick={() => {
@@ -88,12 +119,12 @@ const Timer = () => {
 
                 setSessionLength(sessionLength + 1);
 
-                if (!isRunning && status === "session") {
+                if (status === "focus") {
                   setTime(sessionLength * 60 + 60);
                 }
               }}
             >
-              Up
+              <i className="fa-solid fa-arrow-up" />
             </button>
             <p id="session-length">{sessionLength}</p>
             <button
@@ -103,24 +134,19 @@ const Timer = () => {
 
                 setSessionLength(sessionLength - 1);
 
-                if (!isRunning && status === "session") {
+                if (status === "focus") {
                   setTime(sessionLength * 60 - 60);
                 }
               }}
             >
-              Down
+              <i className="fa-solid fa-arrow-down" />
             </button>
           </div>
         </div>
 
         <div id="break-label">
           <h1>Break Length</h1>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-around",
-            }}
-          >
+          <div style={flex_SpaceAround}>
             <button
               id="break-increment"
               onClick={() => {
@@ -128,12 +154,12 @@ const Timer = () => {
 
                 setBreakLength(breakLength + 1);
 
-                if (!isRunning && status === "break") {
+                if (status === "break") {
                   setTime(breakLength * 60 + 60);
                 }
               }}
             >
-              Up
+              <i className="fa-solid fa-arrow-up" />
             </button>
             <p id="break-length">{breakLength}</p>
             <button
@@ -143,12 +169,12 @@ const Timer = () => {
 
                 setBreakLength(breakLength - 1);
 
-                if (!isRunning && status === "break") {
+                if (status === "break") {
                   setTime(breakLength * 60 - 60);
                 }
               }}
             >
-              Down
+              <i className="fa-solid fa-arrow-down" />
             </button>
           </div>
         </div>
@@ -156,22 +182,19 @@ const Timer = () => {
 
       <div id="timer-wrapper">
         <h1 id="timer-label">{status}</h1>
-        <div id="time-left">{clockify()}</div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-around",
-          }}
-        >
+        <div id="time-left">{clockify(time)}</div>
+        <div style={flex_SpaceAround}>
           <button
             title={isRunning ? "Pause" : "Play"}
             id="start_stop"
-            onClick={start_pause}
+            onClick={play_pause}
           >
-            {isRunning ? "Pause" : "Play"}
+            <i
+              className={isRunning ? "fa-solid fa-pause" : "fa-solid fa-play"}
+            />
           </button>
           <button title="Reset" id="reset" onClick={reset}>
-            Reset
+            <i className="fa-solid fa-rotate" />
           </button>
         </div>
       </div>
@@ -180,9 +203,9 @@ const Timer = () => {
 };
 
 const App = () => {
-  function clicked(event) {
+  const clicked = (event) => {
     event.target.classList.add("link-clicked");
-  }
+  };
 
   return (
     <>
